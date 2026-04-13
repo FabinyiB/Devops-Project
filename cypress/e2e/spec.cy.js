37a9d6d321a1da45b2c1c01b60676f22f74a6bdd
Barnabás Fabinyi <61985983+FabinyiB@users.noreply.github.com>
2026-04-13 22:51:47 +0200

    Init

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,16 @@
+describe('Hello World App', () => {
+  it('displays Hello World!', () => {
+    cy.visit('/');
+    cy.contains('Hello World!');
+  });
+
+  it('has correct background color', () => {
+    cy.visit('/');
+    cy.get('body').should('have.css', 'background-color', 'rgb(173, 216, 230)'); // lightblue
+  });
+
+  it('loads the page', () => {
+    cy.visit('/');
+    cy.url().should('include', '/');
+  });
+});
